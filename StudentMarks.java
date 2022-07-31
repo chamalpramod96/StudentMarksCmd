5bd463afaf176221297de6576b445209273a365c
chamalpramod96 <chamalps21@gmail.com>
2022-07-31 14:36:58 +0530

    Add clear consolel

---
+++ StudentMarks.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class StudentMarks{
+    public static String[][] stArray = new String[0][2];
+
+	public static int[][] stMarksArray = new int[0][4];
+
+	public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
