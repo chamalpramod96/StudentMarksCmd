a336942d774898418db6c168c27d21292524b6d0
chamalpramod96 <chamalps21@gmail.com>
2022-07-31 14:38:49 +0530

    Add Clear Console

--- StudentMarks.java
+++ StudentMarks.java
@@ -9,4 +9,18 @@ class StudentMarks{
     public static void main(String[] args) {
         
     }
+
+    public final static void clearConsole() {
+		try {
+			final String os = System.getProperty("os.name");
+			if (os.contains("Windows")) {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} else {
+				System.out.print("\033[H\033[2J");
+				System.out.flush();
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
